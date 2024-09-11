d24c99553c7d6219221d22ecd1b23f3ffcb0d92f
luffytorao721 <negretecabeza5.6@gmail.com>
2024-09-10 22:54:06 -0500

    alargar menu
    solo agrandar 2 espacios

--- Restaurante/Producto.cs
+++ Restaurante/Producto.cs
@@ -9,10 +9,10 @@ namespace sistemarestaurante
     
     internal class Producto
     {
-        private string[] precio = new string[10] {"10000", "20000", "5000", "20000", "7000", "10000", "2000", "5000", "3000", "8000"};
-        private string[] cantidad = new string[10] { "10", "10", "20", "29", "16", "14", "7", "33", "22", "60"};
-        private string[] nombre = new string[10] { "Sushi", "Ramen", "Filete", "Pollo", "Papas Francesas", "Ensalada Cesar", "Gaseosa", 
-            "Jugo natural", "Helado", "Pizza"};
+        private string[] precio = new string[12] {"10000", "20000", "5000", "20000", "7000", "10000", "2000", "5000", "3000", "8000","20000","25000"};
+        private string[] cantidad = new string[12] { "10", "10", "20", "29", "16", "14", "7", "33", "22", "60","30","10"};
+        private string[] nombre = new string[12] { "Sushi", "Ramen", "Filete", "Pollo", "Papas Francesas", "Ensalada Cesar", "Gaseosa", 
+            "Jugo natural", "Helado", "Pizza", "Bandeja paisa","Langostinos"};
         
         public string[] Precio { get => precio; set => precio = value; }
         public string[] Cantidad { get => cantidad; set => cantidad = value; }

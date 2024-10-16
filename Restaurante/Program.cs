d88ec7ccca0dc08a1ee990bab5c95aa0ab0a25c7
Pupu <Pupu@DESKTOP-I6I2VKL>
2024-10-15 19:24:16 -0500

    limpieza de consola

--- Restaurante/Program.cs
+++ Restaurante/Program.cs
@@ -75,7 +75,7 @@ void Menu()
         switch (opcion)
         {
             case 1: Configurar(); confirmar = false; break; // Configura el programa
-            case 2: RegistrarPedido(); confirmar = false; break; // Registra pedido y factura
+            case 2: RegistrarPedido(); Console.Clear(); confirmar = false; break; // Registra pedido y factura
             case 3: confirmar = false; break; // Salir del menú
             default:
                 // Si la opción no es válida, muestra un mensaje y espera una tecla antes de limpiar la pantalla
@@ -470,7 +470,8 @@ void ImprimirMesas()
                     }                    
                 }
                 byte confirm2;
-                Console.WriteLine("Quieres seguir comprando?");
+                Console.Clear();
+Console.WriteLine("Quieres seguir comprando?");
                 do
                 {
                     Console.WriteLine("(1)Si    (2)No");
@@ -499,7 +500,8 @@ void ImprimirMesas()
                         }
                         Console.WriteLine();
                     }
-                    Console.WriteLine("Oprime una tecla para continuar");
+                    Console.Clear();
+Console.WriteLine("Oprime una tecla para continuar");
                     Console.ReadKey();
                     Menu();
                 }
